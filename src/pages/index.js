db545c12fa4e70dafcfd7a9b619f2e3975c79efa
Jose Alves <jose.tapadas@gmail.com>
2020-03-23 19:49:42 +0100

    initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const CURRENT_STREAM = "https://www.youtube.com/embed/5qap5aO4i9A";
+
+export default () => (
+    <div>
+        <iframe width="560" height="315" src={CURRENT_STREAM} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
+    </div>
+);
