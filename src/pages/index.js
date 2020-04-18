f056c3b8f1716e5c19073b4fad1bd1fed496cd02
José Tapadas Alves <josetapadas@users.noreply.github.com>
2020-04-18 09:55:41 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const CURRENT_STREAM = "https://www.youtube.com/watch?v=ARSVNid-tek";
+const CURRENT_STREAM = "https://www.youtube.com/watch?v=7r6UID6okYw";
 
 class Index extends React.Component {
     componentDidMount() {
